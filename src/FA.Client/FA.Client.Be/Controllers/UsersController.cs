c114c50bfc332f29bf2dcb823dc02e5b1ea79c31
Luka Djukanovic <lukadjukadjukanovic@gmail.com>
2024-08-05 22:01:34 +0200

    stage

---
+++ src/FA.Client/FA.Client.Be/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using FA.Client.Be.Request.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FA.Client.Be.Controllers
+{
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/login")]
+        public IActionResult UsersLogin([FromBody] UsersLoginRequest request)
+        {
+            if(request.Username != "admin" || request.Password != "admin")
+                return BadRequest("Korisnicko ime ili lozinka nisu dobri!");
+
+            return Ok("Pong");
+        }
+    }
+}
---
+++ src/FA.Client/FA.Client.Be/Request/Users/UsersLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FA.Client.Be.Request.Users
+{
+    public class UsersLoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
